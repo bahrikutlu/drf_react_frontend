9bc91ef9a10c3620acc23b9ac62bb0e0954dff78
Bahri Kutlu <63510023+bahrikutlu@users.noreply.github.com>
2021-06-10 15:13:02 -0500

    react frontend for tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Posts from './components/posts';
+import PostLoadingComponent from './components/postLoading';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const PostLoading = PostLoadingComponent(Posts);
+	const [appState, setAppState] = useState({
+		loading: false,
+		posts: null,
+	});
 
+	useEffect(() => {
+		setAppState({ loading: true });
+        const http_scheme = window.location.protocol === "https:" ? "https://drf-react-bahri.herokuapp.com" : "http://127.0.0.1:8000";
+        const apiUrl = http_scheme + '/api/'
+		fetch(apiUrl)
+			.then((data) => data.json())
+			.then((posts) => {
+				setAppState({ loading: false, posts: posts });
+			});
+	}, [setAppState]);
+	return (
+		<div className="App">
+			<h1>Latest Posts</h1>
+			<PostLoading isLoading={appState.loading} posts={appState.posts} />
+		</div>
+	);
+}
 export default App;
